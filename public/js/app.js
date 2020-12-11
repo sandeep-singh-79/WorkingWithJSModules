faf5827eb997fc640615c4fc1e46afa2d7202517
Sandeep Singh <s.sandeep79@ymail.com>
2020-12-11 12:35:27 +0530

    aggregating exports and imports

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,10 @@
-// using IIFE to limit the scope of various items that were previously
-// exposed globally.
-// However, this approach makes it difficult to bubble the requisite
-// items back up the scope.
-
-// default import - can be named anything
-import getSessions from "./sessionRepository.js";
-// named import
-import * as template from "./template.js"
+// default import as well as all the named imports
+import getSessions, {sessionTemplate} from "./sessionRepository.js";
 
 function render() {
     var list = document.querySelector("#sessions");
     if (!list) return;
-    list.innerHTML = template.sessionTemplate(data.listItems);
+    list.innerHTML = sessionTemplate(data.listItems);
   }
 
   var data = {
